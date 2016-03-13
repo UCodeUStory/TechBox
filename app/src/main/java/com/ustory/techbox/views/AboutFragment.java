0f26ae57a464967f672a04a988805aa315368f4f
UCodeUStory <qiyuekoon@foxmail.com>
2016-03-13 21:10:45 +0800

    update Fragment
    update Fragment

--- app/src/main/java/com/ustory/techbox/views/AboutFragment.java
+++ app/src/main/java/com/ustory/techbox/views/AboutFragment.java
@@ -1,9 +1,28 @@
 package com.ustory.techbox.views;
 
+import android.view.View;
+
+import com.ustory.techbox.core.BaseFragment;
+
 /**
  * @ Author: qiyue (ustory)
  * @ Email: qiyuekoon@foxmail.com
  * @ Data:2016/3/6
  */
-public class AboutFragment {
+public class AboutFragment extends BaseFragment {
+
+    @Override
+    protected void initView(View rootView) {
+        
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
