0902451cf8a5c7f02a5b44495843600325920d53
UCodeUStory <qiyuekoon@foxmail.com>
2016-03-21 22:18:51 +0800

    add vitamio
    add vitamio

---
+++ app/src/main/java/com/ustory/techbox/views/VitamioActivty.java
@@ -0,0 +1,47 @@
+package com.ustory.techbox.views;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.administrator.test2.R;
+import com.ustory.techbox.core.BaseAppCompatActivity;
+
+/**
+ * @ Author: qiyue (ustory)
+ * @ Email: qiyuekoon@foxmail.com
+ * @ Data:2016/3/21
+ */
+public class VitamioActivty extends BaseAppCompatActivity {
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.vitamio_activity;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initToolbar(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initListeners() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
