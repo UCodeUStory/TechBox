99d282203f1a3d75d3aae35b191a87de1a49c0c3
UCodeUStory <qiyuekoon@foxmail.com>
2016-04-01 23:08:33 +0800

    add Rxjava
    add Rxjava

---
+++ app/src/main/java/com/ustory/techbox/views/RXJavaActivity.java
@@ -0,0 +1,38 @@
+package com.ustory.techbox.views;
+
+import android.os.Bundle;
+
+import com.ustory.techbox.core.BaseAppCompatActivity;
+
+/**
+ * @ Author: qiyue (ustory)
+ * @ Email: qiyuekoon@foxmail.com
+ * @ Data:2016/4/1
+ */
+public class RXJavaActivity extends BaseAppCompatActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initToolbar(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initListeners() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
